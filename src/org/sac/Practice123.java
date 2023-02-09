e22b50856abd9e0fcdad440c6474ae983ea90004
RajaKing2894 <rajasri2609@gmail.com>
2023-02-09 08:46:43 +0530

    Raja add

--- src/org/sac/Practice123.java
+++ src/org/sac/Practice123.java
@@ -4,6 +4,10 @@ public class Practice123 {
 	public static void main(String[] args) {
 		int a=10,b=20;
 		System.out.println(a+b);
+		System.out.println(a / b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		
 	}
 
 }
