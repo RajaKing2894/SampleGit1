e4db26551bc165ce1794a8049f323eb550b2cbf3
RajaKing2894 <rajasri2609@gmail.com>
2023-02-09 08:11:48 +0530

    Anu Added

--- src/org/sac/Practice123.java
+++ src/org/sac/Practice123.java
@@ -1,6 +1,10 @@
 package org.sac;
 
 public class Practice123 {
+	
+	private void AnuMethod() {
+		System.out.println("Anu...");
+	}
 	public static void main(String[] args) {
 		int a=10,b=20;
 		System.out.println(a+b);
