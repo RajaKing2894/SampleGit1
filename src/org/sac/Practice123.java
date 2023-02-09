2c9aaebe28b560f39bea918ce3c5d9f6876b1858
RajaKing2894 <rajasri2609@gmail.com>
2023-02-09 08:27:47 +0530

    Sam Done

--- src/org/sac/Practice123.java
+++ src/org/sac/Practice123.java
@@ -1,6 +1,11 @@
 package org.sac;
 
 public class Practice123 {
+	
+	int a=200;
+	private void SamMethod() {
+		System.out.println("Sam...");
+	}
 	public static void main(String[] args) {
 		int a=10,b=20;
 		System.out.println(a+b);
